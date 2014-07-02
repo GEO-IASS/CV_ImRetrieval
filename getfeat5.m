429baefdc65e419c3f7222e496ffbce00a994736
wdwind <wangfenghahaha@gmail.com>
2014-07-02 17:56:08 +0200

    second commit

---
+++ getfeat5.m
@@ -0,0 +1,25 @@
+function [feat] = getfeat5(im, D)
+    l = 12;
+    
+    [~, descs] = vl_sift(single(im), 'Levels', l);
+    
+    while isempty(descs)
+        warning('No SIFT descriptor detected! Trying more level...');
+        l = l + 1;
+        [~, descs] = vl_sift(single(im), 'Levels', l);
+    end
+    %size(D')
+    %size(descs)
+    
+    descs = double(descs);
+    descs = bsxfun(@rdivide, bsxfun(@minus, descs, mean(descs, 1)), sqrt(var(descs, [], 1)+10));
+    
+    xx = sum(descs.^2, 1);
+    yy = sum(D.^2, 1);
+    xy = descs' * D;
+    
+    dis = sqrt( bsxfun(@plus, yy, bsxfun(@minus, xx', 2*xy)) );
+    
+    feat = max(bsxfun(@minus, mean(dis, 2), dis), 0);
+    feat = sum(feat, 1);
+end
